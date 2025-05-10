3754aa3d761ec34e94858aec364af67117b35e48
Nedas Jaronis <jaronisnedas@gmail.com>
2025-05-10 12:51:14 -0400

    m

--- PhysicsVis/src/manimtest.py
+++ PhysicsVis/src/manimtest.py
@@ -4,5 +4,5 @@ from manim import *
 class MyScene(Scene):
     def construct(self):
         circle = Circle(color=RED)  # Sets stroke (outline) color
-        circle.set_fill(RED, opacity=0.5)  # Sets fill color and opacity
+        circle.set_fill(GREEN, opacity=0.5)  # Sets fill color and opacity
         self.play(Create(circle))
