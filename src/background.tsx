721c2becf7a2ff6ef536bf7c49a9c22dba381798
Nedas Jaronis <jaronisnedas@gmail.com>
2025-07-26 15:59:01 -0400

    .

--- src/background.tsx
+++ src/background.tsx
@@ -4,14 +4,14 @@ import { OrbitControls, useGLTF } from "@react-three/drei";
 
 const BackgroundModel = () => {
   const { scene } = useGLTF("./white_blue.glb"); // Update this path
-  return <primitive object={scene} scale={0.3} />;
+  return <primitive object={scene} scale={0.6} />;
 };
 
 const BackgroundCanvas = () => {
   return (
     <Canvas
       className="background-canvas"
-      camera={{ position: [0, 0, 5], fov: 45 }}
+      camera={{ position: [0, 0, 10], fov: 45 }}
     >
       <ambientLight intensity={1} />
       <BackgroundModel />
