b618d473f329b7e6642cd434cfbc45086a1a64af
Nedas Jaronis <jaronisnedas@gmail.com>
2025-07-27 01:06:17 -0400

    .

--- src/FormulasPage.tsx
+++ src/FormulasPage.tsx
@@ -12,14 +12,14 @@ const Second: React.FC = () => {
       <h2 id="Ftitle">Formulas:</h2>
       <div className="formula-buttons">
         <a className="PHY2048" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">∫ PHY 2048 - Physics w/ Calc 1</a>
-        <a className="PHY2049" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">∬ PHY 2049 - Physics w/ Calc 2</a>
-        <a className="PHY2053" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">🏃‍♂️ PHY 2053 - Gen Physics 1</a>
-        <a className="PHY2054" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">🌡️ PHY 2054 - Gen Physics 2</a>
-        <a className="PHY3221" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">⚙️ PHY 3221 - Mechanics 1</a>
-        <a className="PHY4222" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">⚙️ PHY 4222 - Mechanics 2</a>
-        <a className="PHY3513" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">🔥 PHY 3513 - Thermal Physics 1</a>
-        <a className="PHY3323" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">⚡ PHY 3323 - Electromagnetism 1</a>
-        <a className="PHY4324" href="../data/formulas/PHY2048.pdf" target="_blank" rel="noopener noreferrer">⚡ PHY 4324 - Electromagnetism 2</a>
+        <a className="PHY2049" href="../data/formulas/PHY2049.pdf" target="_blank" rel="noopener noreferrer">∬ PHY 2049 - Physics w/ Calc 2</a>
+        <a className="PHY2053" href="../data/formulas/PHY2053.pdf" target="_blank" rel="noopener noreferrer">🏃‍♂️ PHY 2053 - Gen Physics 1</a>
+        <a className="PHY2054" href="../data/formulas/PHY2054.pdf" target="_blank" rel="noopener noreferrer">🌡️ PHY 2054 - Gen Physics 2</a>
+        <a className="PHY3221" href="../data/formulas/PHY3221.pdf" target="_blank" rel="noopener noreferrer">⚙️ PHY 3221 - Mechanics 1</a>
+        <a className="PHY4222" href="../data/formulas/PHY4222.pdf" target="_blank" rel="noopener noreferrer">⚙️ PHY 4222 - Mechanics 2</a>
+        <a className="PHY3513" href="../data/formulas/PHY3513.pdf" target="_blank" rel="noopener noreferrer">🔥 PHY 3513 - Thermal Physics 1</a>
+        <a className="PHY3323" href="../data/formulas/PHY3323.pdf" target="_blank" rel="noopener noreferrer">⚡ PHY 3323 - Electromagnetism 1</a>
+        <a className="PHY4324" href="../data/formulas/PHY4324.pdf" target="_blank" rel="noopener noreferrer">⚡ PHY 4324 - Electromagnetism 2</a>
     </div>
     </div>
   </div>
